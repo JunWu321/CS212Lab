8365dc626520180d2bc724305e2ec690b4ec576e
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-01-28 12:30:58 -0500

    Create Main.java

---
+++ Q1/Main.java
@@ -0,0 +1,14 @@
+Write a program to determine what happens if you divide a double by 0.
+
+
+package HelloWorld;
+
+ 
+public class Main {
+
+	public static void main(String[] args) {
+		int a=3;
+		int b=0;
+		System.out.println(a/b);
+	}
+}
