015cdb5be785b57ee4ffce0f643709e9b292e635
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-02-07 19:13:56 -0500

    Create Errors.java

---
+++ Errors.java
@@ -0,0 +1,20 @@
+ //Find the 3 errors.
+
+ 
+	public static String findSingletons(String str){
+		String singletons = "";
+		for (int i = 0; i < str.length(); i++){
+			String letter = str.substring(i, i - 1);
+			int count = 0;
+			for (int j = 0; j < str.length(); i++){
+				if (str.substring(j, j + 1).equalsIgnoreCase(letter)){
+					count++;
+				}
+			}
+			if (count == 1){
+				singletons += letter;
+			}
+		}
+	}
+ 
+ 
