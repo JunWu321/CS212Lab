667f460230b71f587a48819ed3381966e501a6d5
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-02-16 19:43:23 -0500

    Update Main.java

--- Q15/Main.java
+++ Q15/Main.java
@@ -1,3 +1,7 @@
+//Write a method that has an ArrayList of Booleans as a parameter that returns the number of true elements.
+
+
+
 public static int countTrueElements(ArrayList<Boolean> boolList) {
     int count = 0;
     for (boolean bool : boolList) {
