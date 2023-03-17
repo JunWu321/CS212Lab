faff847fb240aa7da56b77f1646ef826342b008f
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-03-16 20:08:34 -0400

    Update Main.java

--- Classroom/Main.java
+++ Classroom/Main.java
@@ -1,14 +1,11 @@
-package classroom;
+package Building;
 
 public class Main {
 
 	 public static void main(String[] args) {
-	        Classroom classroom = new Classroom(101, 10.5, 8.5, false, true);
-	        System.out.println(classroom);
-	        System.out.println("Square footage: " + classroom.getSquareFootage());
-	        classroom.flipLightSwitch();
-	        classroom.openDoor();
-	        System.out.println(classroom);
+	       Building a=new Building();
+	       System.out.println(a.getLargestClassroom());
+	       System.out.println(a.CountRoomsWithLightsOn());
 	    }
 
 }
