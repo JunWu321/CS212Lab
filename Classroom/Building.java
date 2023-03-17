8a94721a01bce5f64bd1fd3b61ff8313aa190064
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-03-16 22:38:42 -0400

    Update Building.java

--- Classroom/Building.java
+++ Classroom/Building.java
@@ -7,7 +7,7 @@ public class Building {
 		//int roomNumber, double length, double width, boolean lightsOn, boolean doorOpen
 	    this.Classrooms = new Classroom[] {new Classroom(2, 3, 4, true, true), new Classroom(3, 4, 5, true, true)};
 		
-		//easier approach below, same as line 8 but more wordy
+//		easier approach below, same as line 8 but more wordy
 // 		this.Classrooms = new Classroom[2] ;
 // 		this.Classrooms[0]=new Classroom(2, 3, 4, true, true);
 // 		this.Classrooms[1]=new Classroom(3, 4, 5, true, true);
