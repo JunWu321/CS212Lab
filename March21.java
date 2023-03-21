b1539f6c6578c3eadb489bf5b97e8f4c444b92df
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-03-21 19:39:10 -0400

    Create March21.java

---
+++ March21.java
@@ -0,0 +1,4 @@
+In class today we will be going through the Card and Deck example classes on BB. For lab today/Monday, you can ask the students to do the following:
+Write isFlush, a static method in the Card class which takes an array of cards (the array can be any size) and returns true if all of the cards in the array have the same suit.
+Write shuffleIn, an instance method in the Deck class that takes another Deck as a parameter, adds all cards from the other Deck into this Deck, shuffles this Deck, and removes all cards from the other Deck.
+Write a good toString method for the Deck class.
