a58229bdc3e35b44a73be221516bf99d4e7ed809
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-03-23 19:31:49 -0400

    Create Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,9 @@
+//  Write the Circle class, a subclass of Shape. Use the PI constant from the Math class in your calculations. Test this by adding a circle to the 
+// list of shapes in the main method.
+
+  
+//    Write two methods in the Circle class called fitsInside, one with a Circle and one with a Rectangle as a parameter. Return true if this 
+//   Circle fits inside the other shape without the sides touching. Test this by seeing if your Circle fits in another Circle, a Rectangle, and a Square.
+
+    
+//      Write a Solid class with methods to return the volume and surface area with subclasses for Sphere, Cube, and Cone.
