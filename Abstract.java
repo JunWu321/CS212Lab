51a83011b352549c988fd83c1e7286cb997a965e
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-03-23 19:56:21 -0400

    Create Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,4 @@
+// Write ConsistentChick, a subclass of Chick. A ConsistentChick decides at birth whether to chirp or cheep and stands by that 
+// decision forever. Its type is still Chick.
+  
+// Write DoubleDog, a subclass of Dog. A DoubleDog says whatever a dog says, but twice in a row. Its type is still Dog./
