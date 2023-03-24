44df198ea7739029a9a8628c5066347766cc9568
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-03-23 20:07:08 -0400

    Update Abstract.java

--- Abstract.java
+++ Abstract.java
@@ -1,4 +1,27 @@
 // Write ConsistentChick, a subclass of Chick. A ConsistentChick decides at birth whether to chirp or cheep and stands by that 
 // decision forever. Its type is still Chick.
+
+package animal;
+
+public class ConsistentChick extends Chick{
+	
+	private final String sound;
+
+	public ConsistentChick() {
+	    super();
+	    if (Math.random() < 0.5) {
+	        sound = "Chirp";
+	    } else {
+	        sound = "Cheep";
+	    }
+	}
+
+	@Override
+	public String getSound() {
+	    return sound;
+	}
+
+}
+
   
 // Write DoubleDog, a subclass of Dog. A DoubleDog says whatever a dog says, but twice in a row. Its type is still Dog./
