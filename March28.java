bbef85f3b266c1de2828567b93e7823092c7d3ca
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-03-28 19:54:23 -0400

    Update March28.java

--- March28.java
+++ March28.java
@@ -1,5 +1,17 @@
 // Add an overlaps method to Range, which determines whether this Range has any numerical overlap with another Range.
   
+
+  public boolean overlaps(Range a) {
+    for (int i = this.low; i <= this.high; i++) {
+      for (int j = a.low; j <= this.high; j++) {
+        if (i == j) {
+          return true;
+        }
+        return false;
+      }
+    }
+  }
+
 // Add an overlaps method to NumberArray, which determines this NumberArray has any overlap with another NumberArray.
   
 // Write the class MultiGroup which implements NumberGroup. A MultiGroup takes a list of NumberGroups as the constructor parameter and 
