c6f33f35a03c787a4dca8fb6714167115578fb8d
JunWu321 <123829909+JunWu321@users.noreply.github.com>
2023-04-25 20:05:56 -0400

    Create Fraction.java

---
+++ Fraction.java
@@ -0,0 +1,45 @@
+//Fraction class implementing Comparable Interface. Add an equals method
+
+//equals returns a boolean BUT compareTo returns an int
+//-1 means left hand side smaller than right hand side
+//0 means left hand side EQUAL to right hand side
+//1 means left hand side larger than right hand side
+
+public class Fraction implements Comparable < Fraction > {
+  public int numerator;
+  public int denominator;
+
+  public Fraction(int numerator, int denominator) {
+    if (denominator == 0) {
+      throw new IllegalArgumentException("Denominator cannot be zero.");
+    }
+
+    this.numerator = numerator;
+    this.denominator = denominator;
+  }
+
+  // The compareTo method is used to compare this Fraction object with another Fraction object.
+  @Override
+  public int compareTo(Fraction other) {
+    //remember, numerator and denominator are ints, so we must cast it.
+	double thisValue = (double) (this.numerator / this.denominator);
+    double otherValue = (double) other.numerator/ other.denominator;
+    return Double.compare(thisValue, otherValue);
+  }
+
+  // The equals method checks if this Fraction object is equal to another Fraction object.
+  public boolean equals(Fraction other) {
+    if (other == null) {
+      return false;
+    }
+    //remember, numerator and denominator are ints, so we must cast it.
+    double thisValue = (double) this.numerator / this.denominator;
+    double otherValue = (double) other.numerator / other.denominator;
+    return thisValue == otherValue;
+  }
+
+  @Override
+  public String toString() {
+    return numerator + "/" + denominator;
+  }
+}
